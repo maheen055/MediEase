5f67add133261a75185781f3c866597698606f03
Maheen Shoaib <144755785+maheen055@users.noreply.github.com>
2023-11-08 23:52:06 -0500

    Update MediEase.cpp
    Configuration of parts.

--- MediEase.cpp
+++ MediEase.cpp
@@ -13,6 +13,25 @@ int buttonPressed = 0;
 int isOn = 0;
 
 void GPIO_Init(void) {
+      GPIO_InitTypeDef GPIO_InitStruct = {0};
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    // Configure the button pin as input with pull-up
+    GPIO_InitStruct.Pin = BUTTON_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    HAL_GPIO_Init(BUTTON_PORT, &GPIO_InitStruct);
+
+    // Configure the motor pin as output
+    GPIO_InitStruct.Pin = MOTOR_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(MOTOR_PORT, &GPIO_InitStruct);
+
+    // Configure the buzzer and LED pins as output
+    GPIO_InitStruct.Pin = BUZZER_PIN | LED_PIN;
+    HAL_GPIO_Init(BUZZER_PORT, &GPIO_InitStruct);
 }
 
 int main(void) {

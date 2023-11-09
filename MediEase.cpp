3b6798a8bd7979c46c6e905904b30d64505e2799
Maheen Shoaib <144755785+maheen055@users.noreply.github.com>
2023-11-08 23:41:00 -0500

    Update MediEase.cpp
    Definition of GPIO pins for the button, motor, buzzer, and LED

---
+++ MediEase.cpp
@@ -0,0 +1,10 @@
+#include "stm32f4xx_hal.h"
+
+#define BUTTON_PIN GPIO_PIN_0
+#define BUTTON_PORT GPIOA
+#define MOTOR_PIN GPIO_PIN_8
+#define MOTOR_PORT GPIOB
+#define BUZZER_PIN GPIO_PIN_9
+#define BUZZER_PORT GPIOB
+#define LED_PIN GPIO_PIN_5
+#define LED_PORT GPIOA
